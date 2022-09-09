2109546f16822834456dd01b38e64f74be404524
amitchoksi <choksiamit@gmail.com>
2022-09-09 11:35:12 -0700

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import scraping
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
+
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+
 mongo = PyMongo(app)
 
 @app.route("/")
@@ -23,4 +25,4 @@ def scrape():
 
 if __name__ == "__main__":
    app.run()
-
+   
